61dc3091dbf4cb91fe3ea25f7dc8a75827b16989
massimo sardo <massimo@megaflora.it>
2018-12-27 14:44:27 +0100

    patch for Geoserver 2.14

--- OmirlServer/Omirl/src/it/fadeout/omirl/AuthService.java
+++ OmirlServer/Omirl/src/it/fadeout/omirl/AuthService.java
@@ -19,7 +19,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 
 @Path("/auth")
 public class AuthService {
@@ -219,7 +218,11 @@ public class AuthService {
 			return oResult;
 		}
 		if (oSettings == null) {
+<<<<<<< remotes/tux/Daemon1.2
 			oResult.StringValue = "Si è verificato un errore si prega di riprovare";
+=======
+			oResult.StringValue = "Si � verificato un errore si prega di riprovare";
+>>>>>>> sync with old repository
 			return oResult;
 		}
 		
